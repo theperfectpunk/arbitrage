894c0443a4e48c0cee3716d60c79bdefff9a2766
Mohit Tokas <mohittokas@live.com>
2021-09-15 04:09:17 +0530

    remove unused code

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
 import express from "express";
-import compression from "compression";  // compresses requests
-import session from "express-session";
+import compression from "compression";
 import bodyParser from "body-parser";
 import lusca from "lusca";
-import MongoStore from "connect-mongo";
-import flash from "express-flash";
 import path from "path";
-import mongoose from "mongoose";
-import passport from "passport";
-import bluebird from "bluebird";
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
@@ -19,17 +12,6 @@ import * as contactController from "./controllers/contact";
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-const mongoUrl = MONGODB_URI;
-mongoose.Promise = bluebird;
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-    // process.exit();
-});
-
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
@@ -37,40 +19,8 @@ app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        mongoUrl,
-        mongoOptions: {
-            autoReconnect: true
-        }
-    })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-    req.path !== "/login" &&
-    req.path !== "/signup" &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    } else if (req.user &&
-    req.path == "/account") {
-        req.session.returnTo = req.path;
-    }
-    next();
-});
 
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
@@ -90,13 +40,5 @@ app.get("/api", apiController.getApi);
 app.get("/api/wazirx", apiController.getWazirX);
 app.get("/api/binance", apiController.getBinance);
 app.get("/api/arbitrage", apiController.getArbitrage);
-
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-    res.redirect(req.session.returnTo || "/");
-});
 
 export default app;
